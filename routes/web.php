065c8b76bea3f0405d2ad705bd4db7184d72f8b2
Ama-Dombawela <amadombawela@gmail.com>
2026-06-01 11:48:47 +0530

    setup web routes for CRM module

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Controllers\InvoiceController;
+use App\Http\Controllers\ProposalController;
+use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,10 +15,27 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+//Profile(breeze deafult)
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+//Customers
+Route::resource('customers', CustomerController::class)->middleware('auth');
+Route::patch('customers/{customer}', [CustomerController::class, 'update'])->name('customers.update')->middleware('auth'); // Update customer status (partial update)
+
+// Proposals
+Route::resource('proposals', ProposalController::class);
+Route::patch('proposals/{proposal}/status', [ProposalController::class, 'changeStatus'])->name('proposals.changeStatus'); // Custom route to change only proposal status (not full update)
+
+// Invoices
+Route::resource('invoices', InvoiceController::class);
+Route::patch('invoices/{invoice}/status', [InvoiceController::class, 'changeStatus'])->name('invoices.changeStatus');
+Route::post('invoices/{invoice}/send', [InvoiceController::class, 'sendInvoice'])->name('invoices.send'); // Send invoice via email
+
+// Transactions
+Route::get('/transactions', [TransactionController::class, 'index'])->name('transactions.index');
+
+require __DIR__ . '/auth.php';
